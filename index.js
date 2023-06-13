c0d31724dcddb920594cecb516bc677d639a1656
Matt Chiaro <131219830+MattChiaro@users.noreply.github.com>
2023-06-12 19:57:58 -0500

    prompts completed

---
+++ index.js
@@ -0,0 +1,24 @@
+const inquirer = require('inquirer');
+const fs = require('fs');
+const SVG = require('./lib/svg');
+const shapes = require('./lib/shapes');
+
+const questions = [
+    {
+        type: 'input',
+        name: 'text',
+        message: 'Please enter up to 3 characters',
+    }, {
+        type: 'input',
+        name: 'textColor',
+        message: 'Choose a color (or hex value) for the text',
+    } , {
+        type: 'list',
+        name: 'shape',
+        message: 'Choose a shape',
+        choices: ['Square', 'Triangle', 'Circle'],
+    }, {
+        type: 'input',
+        name: 'shapeColor',
+        message: 'Choose a color (or hex value) for the shape',
+    }]
