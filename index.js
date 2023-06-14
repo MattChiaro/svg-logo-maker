e9881312495791d19045e322b056f28426254dd2
Matt Chiaro <131219830+MattChiaro@users.noreply.github.com>
2023-06-13 20:58:14 -0500

    genSVG() created and executed

--- index.js
+++ index.js
@@ -23,5 +23,28 @@ const questions = [
         message: 'Choose a color (or hex value) for the shape',
     }]
 
+
+const genSVG = (fileName, svg) => {
+    fs.writeFile(fileName,svg.render(), (err) => {err ? console.log(err) : console.log('.svg file created')});
+}
+
+
 inquirer.prompt(questions).then((answers) => {
-    console.log(answers); })
+    const newSVG = new SVG()
+    newSVG.setText(answers.text, answers.textColor);
+    switch (answers.shape) {
+        case 'Square':
+            newSVG.setShape(new shapes.Square(), answers.shapeColor);
+            break;
+        case 'Triangle':
+            newSVG.setShape(new shapes.Triangle(), answers.shapeColor);
+            break;
+        case 'Circle':
+            newSVG.setShape(new shapes.Circle(), answers.shapeColor);
+            break;
+    }
+    genSVG('./examples/test.svg', newSVG)
+
+})
+
+
